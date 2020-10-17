fbfdfc667d60b0dc19c45b754d6175174cde8683
Mustafa EREN <33000530+coderentr@users.noreply.github.com>
2020-10-17 15:06:14 +0300

    Initial commit

---
+++ SwaggerDemoApp/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwaggerDemoApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpPost]
+        [Route("ReturnCategoryName")]
+        public string CategoryName(string Name)
+        {
+            return Name;
+        }
+    }
+}
---
+++ SwaggerDemoApp/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SwaggerDemoApp.Model;
+
+namespace SwaggerDemoApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("products")]
+        public List<Product> GetProducts()
+        {
+            var products = new List<Product>{
+                
+                new Product {Id=1,Name="Leptop", Price=12000 },
+                new Product{Id=2, Name="Phone", Price=5000 }
+            };
+            return products;
+        }
+
+        [HttpGet]
+        [Route("GetProductById")]
+        public Product ReturnProduct(int Id)
+        {
+            var products = new List<Product>{
+
+                new Product {Id=1,Name="Leptop", Price=12000 },
+                new Product{Id=2, Name="Phone", Price=5000 }
+            };
+            var product = products.FirstOrDefault(p => p.Id == Id);
+            return product;
+        }
+    }
+}
